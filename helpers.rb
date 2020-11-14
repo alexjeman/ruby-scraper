6f6c20a0552c18617e084fcd96b69c4099b96903
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-15 00:22:25 +0200

    Update accounts method

--- helpers.rb
+++ helpers.rb
@@ -1,4 +1,3 @@
-require 'securerandom'
 require 'json'
 
 class Accounts
@@ -9,7 +8,6 @@ class Accounts
   def add_account(type, name, bank, currency, nature)
     account_data = load_file
     new_account = {
-      'id' => SecureRandom.uuid,
       'type' => type,
       'bank' => bank,
       'name' => name,
@@ -20,6 +18,14 @@ class Accounts
 
     account_data['accounts'].append(new_account)
     save_file(account_data)
+  end
+
+  def update_account(name, key, value)
+    accounts = load_file['accounts']
+    current_acc = accounts.select { |account| account['name'] == name }.tap { |account| accounts -= account }
+    current_acc.first[key] = value
+    accounts.append(current_acc.first)
+    save_file({ 'accounts' => accounts })
   end
 
   def save_file(account_data)
@@ -36,6 +42,7 @@ class Accounts
   end
 end
 
-# account_test = Accounts.new
-#
-# account_test.add_account('demo', 'testacc1', 'demo', 'MDL', 'credit_card')
+account_test = Accounts.new
+
+# account_test.add_account('demo', 'testacc2', 'demo', 'MDL', 'credit_card')
+# account_test.update_account('testacc1', 'balance', 710)
